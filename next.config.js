5e7fbcc9f5959d4d174fe5384db8c5120fc92f84
François Hodierne <francois@hodierne.net>
2019-10-04 13:45:47 +0200

    dx: keep next.js pages longer in buffer

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const withCSS = require('@zeit/next-css');
 const withSourceMaps = require('@zeit/next-source-maps');
 
 const nextConfig = {
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 60 * 60 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 10,
+  },
   webpack: (config, { webpack }) => {
     config.plugins.push(
       // Ignore __tests__
